2f799205d5fca1991cba4740cac194fe306cd50f
rosadl <rosadl84@gmail.com>
2017-07-15 17:23:28 +0200

    first

---
+++ astroevents/routes/auth-routes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const authRoutes = express.Router();
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+
+
+const User = require("../models/User");
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      message: "Indicate username and password"
+    });
+    return;
+  }
+
+  User.findOne({
+    username
+  }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        message: "The username already exists"
+      });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", {
+          message: "Something went wrong"
+        });
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
+});
+
+authRoutes.post("/login", passport.authenticate("local", {
+  successRedirect: "/event-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+authRoutes.get("/event-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("events", { user: req.user });
+});
+
+authRoutes.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+module.exports = authRoutes;
